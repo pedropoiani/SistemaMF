2720747b0dedbe4a03d406b8702906ed4e8812a4
Pedro H. Poiani <pedropoiani@gmail.com>
2024-05-27 10:35:08 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,4 +25,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 })
 
-app.listen(3333, () => console.log('Servidor rodando na porta 3333!'))
+app.listen(3333, () => console.log('Servidor rodando na porta 3333!!'))
